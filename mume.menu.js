8aec0485267e799073e54613d618ddb686f117cc
Waba <waba4mume@users.noreply.github.com>
2017-11-19 22:53:54 +0100

    make the map panel detachable

--- mume.menu.js
+++ mume.menu.js
@@ -1,13 +1,10 @@
-/* This is an example of how to add custom menu entries using the current crude
- * extension mechanism. */
-
 toolbar_menus[MENU_HELP][MI_SUBMENU].unshift(
     'New to MUME?', 'mume_menu_new();',
     'MUME Help',    'mume_menu_help();',
     'MUME Rules',   'mume_menu_rules();' );
 
-toolbar_menus[MENU_FILE][MI_SUBMENU].unshift(
-    'MUME Map', 'open_mume_map_window();' );
+toolbar_menus[MENU_OPTIONS][MI_SUBMENU].unshift(
+    'Detach Map', 'open_mume_map_window();' );
 
 function mume_menu_new()
 {
@@ -26,13 +23,15 @@ function mume_menu_rules()
 
 function open_mume_map_window()
 {
-    var mapWindow, connectWindows;
-
-    mapWindow = window.open( "map.html", "mume_map", "dialog,minimizable,width=820,height=620" );
-    if ( mapWindow === null )
+    globalMapWindow = window.open( "map.html", "mume_map", "dialog,minimizable,width=820,height=620" );
+    if ( globalMapWindow === null )
     {
         alert( "Your browser refused to open the map window, you have to allow it "
             +"somewhere near the top right corner of your screen. Look for a "
             +"notification about blocking popups." );
+        return;
     }
+
+    if ( globalSplit )
+        globalSplit.collapse( 1 );
 }
